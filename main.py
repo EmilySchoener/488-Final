6d39d14ec2eb58e9f875964f4e2efe64d7d6933e
Emily Schoener <ers0025@uah.edu>
2024-04-10 12:35:19 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('Video Games Sales.csv')
 
 df = data
-print(df.head(5))
+#print(df.head(5))
+
+#print(df.info)
+df = df.dropna(axis=0) #Drop null values
+#print(df.info)
+
+print(df.columns)
+print(df['Platform'].unique())
+print(df['Genre'].unique())
