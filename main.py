551bd0f850f731e8cf7d8f52f47c857fd35151db
Emily Schoener <ers0025@uah.edu>
2024-04-16 09:56:25 -0500

    Preprocessing and insight graphs.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import seaborn as sns
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, Lasso, LassoCV
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score
 from tabulate import tabulate
@@ -10,23 +14,123 @@ from tabulate import tabulate
 # reads initial file
 data = pd.read_csv('Video Games Sales.csv')
 df = data
-df = df.dropna(axis=0)  # Drop null values
 
 # removes all columns except platform, genre, and global sales
 df = df.drop(labels='index', axis=1)
-df = df.drop(labels='Rank', axis=1)
+#df = df.drop(labels='Rank', axis=1)
 df = df.drop(labels='Game Title', axis=1)
-df = df.drop(labels='Year', axis=1)
-df = df.drop(labels='Publisher', axis=1)
-df = df.drop(labels='North America', axis=1)
-df = df.drop(labels='Europe', axis=1)
-df = df.drop(labels='Japan', axis=1)
-df = df.drop(labels='Rest of World', axis=1)
-df = df.drop(labels='Global', axis=1)
+#df = df.drop(labels='Year', axis=1)
+#df = df.drop(labels='Publisher', axis=1)
+#df = df.drop(labels='North America', axis=1)
+#df = df.drop(labels='Europe', axis=1)
+#df = df.drop(labels='Japan', axis=1)
+#df = df.drop(labels='Rest of World', axis=1)
+#df = df.drop(labels='Global', axis=1)
 
-# print(df.columns)
+print(df.columns)
+#print ('The train data has {0} rows and {1} columns'.format(df.shape[0],df.shape[1]))
 # print(df['Platform'].unique())
 # print(df['Genre'].unique())
+
+#identifying datatypes for columns
+'''
+objects_cols = ['object']
+objects_lst = list(df.select_dtypes(include=objects_cols).columns)
+print("Total number of categorical columns are ", len(objects_lst))
+print("There names are as follows: ", objects_lst)
+
+int64_cols = ['int64']
+int64_lst = list(df.select_dtypes(include=int64_cols).columns)
+print("Total number of numerical columns are ", len(int64_lst))
+print("There names are as follows: ", int64_lst)
+
+float64_cols = ['float64']
+float64_lst = list(df.select_dtypes(include=float64_cols).columns)
+print("Total number of float64 columns are ", len(float64_lst))
+print("There name are as follow: ", float64_lst)
+
+#count the total number of rows and columns.
+print('The new dataset has {0} rows and {1} columns'.format(df.shape[0],df.shape[1]))
+'''
+#print(df.isna().sum()) #Identify where null values are
+df.dropna(inplace=True)  # Drop null values
+
+#Encoding categorical data values
+le = LabelEncoder()
+df.Platform = le.fit_transform(df.Platform)
+df.Genre = le.fit_transform(df.Genre)
+df.Publisher = le.fit_transform(df.Publisher)
+
+#Graph data before fixing skew
+df.hist(bins=50, figsize=(10,10))
+plt.suptitle("Data before fixing skew")
+plt.show()
+
+#Fuction to identify skewness
+def right_nor_left(df, float64_lst):
+    temp_skewness = ['column', 'skewness_value', 'skewness (+ve or -ve)']
+    temp_skewness_values = []
+
+    temp_total = ["positive (+ve) skewed", "normal distrbution", "negative (-ve) skewed"]
+    positive = 0
+    negative = 0
+    normal = 0
+
+    for value in float64_lst:
+
+        rs = round(df[value].skew(), 4)
+
+        if rs > 0:
+            temp_skewness_values.append([value, rs, "positive (+ve) skewed"])
+            positive = positive + 1
+
+        elif rs == 0:
+            temp_skewness_values.append([value, rs, "normal distrbution"])
+            normal = normal + 1
+
+        elif rs < 0:
+            temp_skewness_values.append([value, rs, "negative (-ve) skewed"])
+            negative = negative + 1
+
+    skewness_df = pd.DataFrame(temp_skewness_values, columns=temp_skewness)
+    skewness_total_df = pd.DataFrame([[positive, normal, negative]], columns=temp_total)
+
+    return skewness_df, skewness_total_df
+
+float64_cols = ['float64']
+float64_lst_col = list(df.select_dtypes(include=float64_cols).columns)
+
+skew_df, skew_total_df = right_nor_left(df, float64_lst_col)
+print(skew_df) #The sales data is skewed
+
+df['NA_Sales_up'] = np.sqrt(df['North America'])
+print(df['NA_Sales_up'].skew())
+
+df['EU_Sales_up'] = np.sqrt(df['Europe'])
+print(df['EU_Sales_up'].skew())
+
+df['JP_Sales_up'] = np.sqrt(df['Japan'])
+print(df['JP_Sales_up'].skew())
+
+df['Other_Sales_up'] = np.sqrt(df['Rest of World'])
+print(df['Other_Sales_up'].skew())
+
+df['Global_Sales_up'] = np.sqrt(df['Global'])
+print(df['Global_Sales_up'].skew())
+
+#Drop old columns
+df = df.drop(['North America','Europe','Japan','Rest of World','Global'],axis = 1)
+
+#Graph data after fixing skew
+df.hist(bins=50, figsize=(10,10))
+plt.suptitle("Data after fixing skew")
+plt.show()
+
+#correlation plot
+sns.set(rc = {'figure.figsize':(7,7)})
+corr = df.corr().abs()
+sns.heatmap(corr,annot=True)
+plt.show()
 
 
 # converts values to an index for fitting / calculations
@@ -35,7 +139,6 @@ def game_scoring(value):
         return 1
     else:
         return 0
-
 
 # converts values to an index for fitting / calculations
 def platform(value):
@@ -112,7 +215,6 @@ def genre(value):
     elif value == 'Strategy':
         return 11
 
-
 # sets up target and data
 target_df = pd.DataFrame(data=df, columns=['Review'])
 target_df['Review'] = target_df['Review'].apply(game_scoring)
@@ -157,7 +259,7 @@ for i in range(len(platforms)):
 
 # prints out final results
 array.append(row)
-print(tabulate(array, headers=genres, tablefmt="grid"))
+#print(tabulate(array, headers=genres, tablefmt="grid"))
 
 # Naive Bayes unused (low accuracy)
 '''
@@ -186,3 +288,5 @@ print("Accuracy:", accuray)
 print("F1 Score:", f1)
 
 '''
+
+#
